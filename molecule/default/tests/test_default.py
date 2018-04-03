d4706c7d46f73b9dfeeb0322c438adee04897f2a
Christopher Torgalson <manager@bedlamhotel.com>
2018-04-03 22:40:57 +0200

    Adds first test for apache modules
    - Currently only tests for `rewrite`.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,14 +1,16 @@
 import os
 
 import testinfra.utils.ansible_runner
+import pytest
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+@pytest.mark.parametrize("name", [
+    "rewrite",
+])
+def test_apache_modules(host, name):
+    mods = host.run("apache2ctl -M")
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert name in mods.stdout
